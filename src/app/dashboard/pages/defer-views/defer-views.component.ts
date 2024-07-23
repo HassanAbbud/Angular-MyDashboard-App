5606925b3cefbc5f2ffda98782d3f2bc13f8a241
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-23 16:16:30 -0600

    Defer Blocks

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../shared/title/title.component';
+import { HeavyLoadersSlowComponent } from '../../shared/heavy-loaders/heavy-loaders-slow.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, HeavyLoadersSlowComponent, CommonModule],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
