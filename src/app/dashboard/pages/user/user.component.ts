c349b12c24cf1092f90704537623a4ff6349505b
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-24 09:06:48 -0600

    Observable to signal with toSignal()

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TitleComponent } from '../../shared/title/title.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './user.component.html',
   styles: ``
 })
 export class UserComponent {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  // public user = signal<User| undefined>(undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `User info: ${this.user()?.first_name} ${
+        this.user()?.last_name
+      } `;
+    }
+
+    return 'User info';
+  });
 }
--- src/app/dashboard/services/users.service.ts
+++ src/app/dashboard/services/users.service.ts
@@ -1,7 +1,7 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
-import { User, UsersResponse } from '../interfaces/req-response';
+import { User, UserResponse, UsersResponse } from '../interfaces/req-response';
 import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs';
+import { delay, map } from 'rxjs';
 
 interface State {
   users: User[];
@@ -31,8 +31,15 @@ export class UsersService {
           loading: false,
           users: res.data,
         })
-
       });
+  }
+
+  getUserById( id: string ) {
+    return this.http.get<UserResponse>(`https://reqres.in/api/users/${ id }`)
+      .pipe(
+        delay(1500),
+        map( resp => resp.data )
+      )
 
   }
 }
