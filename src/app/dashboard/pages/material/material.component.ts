fa6964818d1aaa591ca09ef71e532654dbd10175
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-24 11:57:59 -0600

    Angular material components

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './material.component.html',
   styles: ``
 })
